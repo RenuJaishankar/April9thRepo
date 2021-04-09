57c98135422af25f3cd73cbd2e694924926a3387
Renu Jaishankar <Renu.Jaishankar@yahoo.com>
2021-04-09 06:33:08 -0400

    first commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Please enter the string");
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;sc.hasNext()== true; i++){
+
+            String a = sc.nextLine();
+            System.out.println(i +" "+ a);}
+    }
+}
+
